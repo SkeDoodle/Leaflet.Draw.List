700d2e6afba81ec165d93adcdf9acfbdd5cfedfc
Thomas <thomas.neger@incotec.fr>
2016-07-21 16:13:08 +0200

    typeLayer value `marker` is the same for all the markers

--- src/Toolbar.List.js
+++ src/Toolbar.List.js
@@ -11,9 +11,9 @@ L.Toolbar.List = L.Toolbar.extend({
     this._map = map;
 
     for (i = 0; i < modeHandlers.length; i++) {
-      modeHandlers[i].handler.type = modeHandlers[i].handler.type + i;
       if (modeHandlers[i].enabled) {
         this._initModeHandler(
+            i,
             modeHandlers[i].handler,
             this._toolbarContainer,
             buttonIndex++,
@@ -39,5 +39,47 @@ L.Toolbar.List = L.Toolbar.extend({
     container.appendChild(this._actionsContainer);
 
     return container;
+  },
+
+  _initModeHandler: function (id, handler, container, buttonIndex, classNamePredix, buttonTitle) {
+
+    var type = handler.type;
+
+    this._modes[id] = {};
+
+    this._modes[id].handler = handler;
+    this._modes[id].handler.id = id;
+
+    this._modes[id].button = this._createButton({
+      type: type,
+      title: buttonTitle,
+      className: classNamePredix + '-' + type,
+      container: container,
+      callback: this._modes[id].handler.enable,
+      context: this._modes[id].handler
+    });
+
+    this._modes[id].buttonIndex = buttonIndex;
+
+    this._modes[id].handler
+        .on('enabled', this._handlerActivated, this)
+        .on('disabled', this._handlerDeactivated, this);
+  },
+
+  _handlerActivated: function (e) {
+
+    var index = e.target.id;
+
+    // Disable active mode (if present)
+    this.disable();
+
+    // Cache new active feature
+    this._activeMode = this._modes[index];
+
+    L.DomUtil.addClass(this._activeMode.button, 'leaflet-draw-toolbar-button-enabled');
+
+    this._showActionsToolbar();
+
+    this.fire('enable');
   }
 });
